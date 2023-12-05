5a7141dc07ea30ecd15b32b6bf9d42ac9c394bef
11 <1@>
2023-12-05 17:53:30 +0800

    commit

--- src/main/java/com/pond/build/FishTestApplication.java
+++ src/main/java/com/pond/build/FishTestApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.pond.build.mapper")
 @EnableTransactionManagement
+@EnableCaching
 public class FishTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
