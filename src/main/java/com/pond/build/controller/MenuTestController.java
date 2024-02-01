c685bdbc1fc7ab467648bc60c6b5da4b900528b5
“hollow288” <571339274@qq.com>
2024-02-01 18:01:03 +0800

    commit

--- src/main/java/com/pond/build/controller/MenuTestController.java
+++ src/main/java/com/pond/build/controller/MenuTestController.java
@@ -2,15 +2,21 @@ package com.pond.build.controller;
 
 
 import com.pond.build.service.CommonService;
+import com.pond.build.utils.MinioUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Value;
 import javax.swing.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class MenuTestController {
@@ -47,4 +53,39 @@ public class MenuTestController {
         logger.info("task1方法耗时 {} ms" , t2-t1);
         return "hello";
     }
+
+
+
+
+    @Autowired
+    private MinioUtil minioUtil;
+    @Value("${minio.endpoint}")
+    private String address;
+    @Value("${minio.bucketName}")
+    private String bucketName;
+
+    @PostMapping("/upload")
+    public String upload(MultipartFile file,String filePath) {
+        List<String> upload = minioUtil.upload(new MultipartFile[]{file},filePath);
+        return address + "/" + bucketName + "/" +filePath+ upload.get(0);
+    }
+
+    @PostMapping("/uploadList")
+    public List<String> uploadList(MultipartFile[] file) {
+        List<String> upload = minioUtil.upload(file);
+        upload = upload.stream().map(it -> address + "/" + bucketName + "/" + it).collect(Collectors.toList());
+        return upload;
+    }
+
+    @GetMapping("/getUrl")
+    public String get(String objectName) {
+        return minioUtil.getUploadObjectUrl(objectName);
+
+    }
+    @GetMapping("/down")
+    public ResponseEntity<byte[]> down(String objectName) {
+        return minioUtil.download(objectName);
+    }
+
+
 }
